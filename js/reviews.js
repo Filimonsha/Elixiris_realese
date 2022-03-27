26ce46a46f4ca634665655751599f5cee390bb45
Filimonsha <Filimonshacs@mail.ru>
2022-03-27 18:05:07 +0300

    fix yesterday problems

--- js/reviews.js
+++ js/reviews.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $(".reviews__slider").slick({
+    // centerPadding: "600px",
     slidesToShow: 1,
     prevArrow: $(".slider__prev"),
     nextArrow: $(".slider__next"),
